940b4036c7b0ae20e5c171c3eea22a259982a814
Sagan Sutherland <sagan@farthinghalearms.com>
2025-08-27 12:18:43 -0400

    Add documentation to types.py

--- src/ssut_plots/types.py
+++ src/ssut_plots/types.py
@@ -8,6 +8,19 @@ Volume = T.Literal[25, 50]
 
 
 class OneP:
+    """
+    The 1P (one parameter) set of CAMELS changes only one pararameter at a time, all running on the same initial conditions.
+    There are currently 28 parameters which are varried, and each parameter has 5 possible values.
+
+    Parameters
+    ----------
+    param_number
+        The parameter being varied.
+        Must be an integer in the range [1, 28].
+    variation
+        Which value of the parameter.
+        Can be an integer in the range [-2, 2] or a corresponding string ["n2", "n1", "0", "1", "2"].
+    """
     def __init__(
         self,
         param_number: int,
@@ -29,6 +42,16 @@ class OneP:
 
 
 class CV:
+    """
+    The CV (cosmic variance) set of CAMELS does not change any parameters.
+    Instead, only the initial conditions are varied.
+
+    Parameters
+    ----------
+    run_number
+        The run in the CV set to consider.
+        Must be an integer in the range [0, 26].
+    """
     def __init__(self, run_number) -> None:
         if run_number not in range(27):
             raise ValueError("The CV set ranges from 0 to 26")
@@ -39,6 +62,15 @@ class CV:
 
 
 class LH:
+    """
+    The LH (latin hypercube) set of CAMELS changes 6 parameters at once.
+
+    Parameters
+    ----------
+    run_number
+        The run in the LH set to consider.
+        Must be an integer in the range [0, 999].
+    """
     def __init__(self, run_number) -> None:
         if run_number not in range(1000):
             raise ValueError("The LH set ranges from 0 to 999")
